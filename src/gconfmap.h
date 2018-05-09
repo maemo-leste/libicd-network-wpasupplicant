68f5a85b96b37faef0593f28ad5394d65309a8d7
Merlijn Wajer <merlijn@wizzup.org>
2018-05-10 00:56:25 +0200

    gconfmap: initial stub

---
+++ src/gconfmap.h
@@ -0,0 +1,128 @@
+/***********************************************************************************
+*  libicd-network-wpasupplicant: Open source implementation of wpa_supplicant
+*  integration icd2 on Maemo Leste
+*  Copyright (C) 2018 Merlijn B. W. Wajer
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+***********************************************************************************/
+
+/* This file implements reading an entire gconf network structure into a C
+ * structure. In the near future, it will likely also map the gconf network
+ * details to a structure that wpaicd (wpa_supplicant code) will understand.
+ * Some code/headers should probably be shared with connui-wlan and other
+ * packages.*/
+
+
+#ifndef _GCONFMAP_H_
+#define _GCONFMAP_H_
+
+#include <osso-ic-gconf.h>
+
+/*
+TODO:
+   See:
+
+   - connui-wlan/wizard/wlan.c -- perhaps share code or defines?
+   - http://maemo.org/community/maemo-developers/information_required_to_replace_maemo5_wlan_bits/
+
+   Missing keys:
+   - wlan_tx_power
+   - nai
+   - temporary
+   - wlan_hidden
+   - powersave_after_scan
+   - wlan_powersave
+   - EAP_GTC_identity (icw EAP_GTC_passcode)
+   - EAP_SIMPLE_CONFIG_device_password
+   - EAP_manual_username
+   - EAP_use_manual_username
+   - TLS_server_authenticates_client_policy_in_client
+*/
+
+typedef struct {
+    /* if (get_iap_config_bool(ctx->gconf_client, NULL, * "allow_wep_ciphers_in_WPA", FALSE)) */
+    gboolean allow_wep_ciphers_in_WPA;
+
+} GconfNetworkPolicies;
+
+
+typedef struct {
+    /* get_iap_config_bool(ctx->gconf_client, iap_name, "EAP_MSCHAPV2_password_prompt", FALSE); */
+    gboolean EAP_MSCHAPV2_password_prompt;
+
+    /* get_iap_config_string(ctx->gconf_client, iap_name, "EAP_MSCHAPV2_password"); */
+    gboolean EAP_MSCHAPV2_password;
+
+    /* get_iap_config_bool(ctx->gconf_client, iap, "EAP_wpa2_only_mode", FALSE); */
+    gboolean EAP_wpa2_only_mode;
+
+    /* get_iap_config_int(ctx->gconf_client, iap_name, "EAP_default_type"); */
+    gint EAP_default_type;
+
+    /* get_iap_config_int(ctx->gconf_client, iap_name, "PEAP_tunneled_eap_type"); */
+    gint PEAP_tunneled_eap_type;
+
+    /* get_iap_config_string(ctx->gconf_client, iap_name, "EAP_GTC_passcode"); */
+    char* EAP_GTC_passcode;
+
+    /* get_iap_config_string(ctx->gconf_client, iap_name, "EAP_TLS_PEAP_client_certificate_file"); */
+    char* EAP_TLS_PEAP_client_certificate_file;
+} GconfNetworkWPAEAP 
+
+typedef struct {
+    /* key = get_iap_config_string(ctx->gconf_client, ctx->iap_name, keyname); */
+    char* wlan_wepkey1;
+    char* wlan_wepkey2;
+    char* wlan_wepkey3;
+    char* wlan_wepkey4;
+
+    /* default_key = get_iap_config_int(ctx->gconf_client, ctx->iap_name, * "wlan_wepdefkey"); */
+    gint wlan_wepdefkey;
+} GconfNetworkWEP;
+
+typedef struct {
+    char* EAP_wpa_preshared_passphrase;
+} GconfNetworkWPAPSK;
+
+typedef struct {
+    /* channel = get_iap_config_int(ctx->gconf_client, ctx->iap_name, * "wlan_adhoc_channel"); */
+    gint wlan_adhoc_channel;
+} GconfNetworkAdhoc;
+
+typedef struct {
+    /* One-of: "WLAN_INFRA", "WLAN_ADHOC" */
+    char* type;
+
+    /* get_iap_config_string(ctx->gconf_client, ctx->iap_name, "wlan_security"); */
+    /* One-of: "NONE", "WEP", "WPA_PSK", "WPA_EAP" */
+    char* wlan_security;
+
+    /* get_iap_config_bytearray(ctx->gconf_client, ctx->iap_name, "wlan_ssid"); */
+    char* wlan_ssid;
+
+    GconfNetworkWPAEAP wpaeap_config;
+    GconfNetworkWPAPSK wpapsk_config;
+    GconfNetworkWEP wep_config;
+    GconfNetworkAdhoc adhoc_config;
+    GconfNetworkPolicies policy_config;
+} GconfNetwork;
+
+
+GconfNetwork get_gconf_network(GConfClient *client, const char* name);
+void gconf_network_to_XYZ(void);
+
+
+#endif /* _GCONFMAP_H_ */
