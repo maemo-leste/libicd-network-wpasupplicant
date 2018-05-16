a0ac7a4d3710945af63574d5d2f9f762b7d19432
Merlijn Wajer <merlijn@wizzup.org>
2018-05-16 11:13:57 +0200

    minor cleanups & copyright notice

--- src/wlan.c
+++ src/wlan.c
@@ -1,4 +1,27 @@
+/***********************************************************************************
+*  libicd-network-wpasupplicant: Open source implementation of wpa_supplicant
+*  integration icd2 on Maemo Leste
+*  Copyright (C) 2018 Merlijn B. W. Wajer
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+***********************************************************************************/
+
 /**
+ * Some (but most is gone) code taken from older versions, which has:
+ *
  * Copyright (C) 2007 Nokia Corporation. All rights reserved.
  *
  * @author patrik.flykt@nokia.com
@@ -28,11 +51,6 @@
 
 #include <glib.h>
 #include <dbus/dbus.h>
-
-#if 0
-#include <wlancond.h>
-#include <wlancond-dbus.h>
-#endif
 
 #include <osso-ic.h>
 #include <osso-ic-dbus.h>
