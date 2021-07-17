cbe2f3606323f9426eeccffc4565ef6f829f8ba2
Merlijn Wajer <merlijn@wizzup.org>
2021-07-17 17:48:11 +0200

    wlan: using libwpa_client scanning again

--- src/wlan.c
+++ src/wlan.c
@@ -665,15 +665,9 @@ static void wlan_start_search(const gchar * network_type,
 
     /* This is broken in several ways, first of all libwpa_client randomly
      * returns failure when actually everything went fine. And then we have to
-     * ignore it's bad results, which means we cannot fall back to dbus. On top
-     * of that, the n900 can *only* scan using dbus, the libwpa_client / wpa_cli
-     * scan commands just ... fail with -EINVAL */
-#if 0
+     * ignore it's bad results, which means we cannot fall back to dbus.
+     */
     int ret = try_scan_wpa_control(ctx);
-    if (ret != 0)
-        ret = wpaicd_initiate_scan();
-#endif
-    int ret = wpaicd_initiate_scan();
     if (ret == 0)
         goto done;
 
