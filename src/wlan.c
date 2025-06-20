maemo-leste/libicd-network-wpasupplicant

commit 23a6e250faa178de87f96043b396b17170c58b77

ignore "disconnected" events unless we are connected
We may receive a couple of "disconnected" events even before we start
associating, ignore them.

Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

src/wlan.c
@@ -436,19 +436,19 @@ static void wlan_state_change_cb(const char *state, void *data)
 
         ctx->g_association_timer = g_timeout_add_seconds(30, wlan_associate_timeout, (void*)ctx);
     } else if (strcmp(state, "disconnected") == 0) {
-        wlan_set_state(ctx, STATE_IDLE);
 
-        ctx->close_cb(ICD_NW_ERROR,
-                      ICD_DBUS_ERROR_NETWORK_ERROR,
-                      ctx->stored_network_type,
-                      ctx->stored_network_attrs, ctx->stored_network_id);
+        if (ctx->state == STATE_CONNECTED) {
+            wlan_set_state(ctx, STATE_IDLE);
 
-        ctx->stored_network_type = NULL;
-        ctx->stored_network_id = NULL;
-        ctx->stored_network_attrs = 0;
+            ctx->close_cb(ICD_NW_ERROR,
+                          ICD_DBUS_ERROR_NETWORK_ERROR,
+                          ctx->stored_network_type,
+                          ctx->stored_network_attrs, ctx->stored_network_id);
 
-        if (ctx->g_association_timer)
-            g_source_remove(ctx->g_association_timer);
+            ctx->stored_network_type = NULL;
+            ctx->stored_network_id = NULL;
+            ctx->stored_network_attrs = 0;
+        }
     } else if (strcmp(state, "inactive") == 0) {
         if (ctx->g_association_timer)
             g_source_remove(ctx->g_association_timer);

commit 5243c2656ff490755cf083b4e1246487262bfa06

src/wlan.c: Ignore disconnection events only when scanning
Fixes: 23a6e250faa1 ('ignore "disconnected" events unless we are connected')

src/wlan.c
@@ -437,7 +437,7 @@ static void wlan_state_change_cb(const char *state, void *data)
         ctx->g_association_timer = g_timeout_add_seconds(30, wlan_associate_timeout, (void*)ctx);
     } else if (strcmp(state, "disconnected") == 0) {
 
-        if (ctx->state == STATE_CONNECTED) {
+        if (!ctx->scanning) {
             wlan_set_state(ctx, STATE_IDLE);
 
             ctx->close_cb(ICD_NW_ERROR,
@@ -448,6 +448,9 @@ static void wlan_state_change_cb(const char *state, void *data)
             ctx->stored_network_type = NULL;
             ctx->stored_network_id = NULL;
             ctx->stored_network_attrs = 0;
+
+            if (ctx->g_association_timer)
+              g_source_remove(ctx->g_association_timer);
         }
     } else if (strcmp(state, "inactive") == 0) {
         if (ctx->g_association_timer)